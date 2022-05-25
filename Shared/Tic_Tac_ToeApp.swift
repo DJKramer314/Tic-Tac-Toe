0855dfd554b4c2b0c8a282d6acaea5388d2afb93
DJKramer314 <djkramer314@gmail.com>
2022-05-25 09:02:21 -0500

    Initial Commit

---
+++ Shared/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Shared
+//
+//  Created by Dylan on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
